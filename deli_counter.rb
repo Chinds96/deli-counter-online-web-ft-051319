c07c2daef4516077ffe64d2921abdea6f2e9e8d6
Christopher Hinds <christopherhinds96@gmail.com>
2019-05-24 00:46:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length >= 1 
+    katz_deli.map.with_index(1) do |num, name|
+      puts "The line is currently: #{num}. #{name}"
+    end
+  elsif 
+    puts "The line is empty"
+end 
